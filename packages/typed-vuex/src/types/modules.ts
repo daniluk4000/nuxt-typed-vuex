286362371a4a408ed0c3c6049ff2b1da603e3507
daniluk4000 <daniluk4000@outlook.com>
2022-10-05 19:34:47 +0300

    Fix how Webstorm resolves non-dynamic stores

--- packages/typed-vuex/src/types/modules.ts
+++ packages/typed-vuex/src/types/modules.ts
@@ -5,5 +5,5 @@ export type NuxtModules = Record<string, Partial<NuxtStore & {dynamic: boolean}>
 type TransformedModule<T extends NuxtModules, P extends keyof T, O = string> = MergedStoreType<T[P] & BlankStore, O>
 
 export type ModuleTransformer<T, O = string> = T extends NuxtModules
-  ? { [P in keyof T]: T[P]['dynamic'] extends boolean ? undefined | TransformedModule<T, P, O> : TransformedModule<T, P, O> }
+  ? { [P in keyof T]: 'dynamic' extends keyof T[P] ? undefined | TransformedModule<T, P, O> : TransformedModule<T, P, O> }
   : {}
